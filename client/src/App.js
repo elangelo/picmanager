55760457cd55a11817659c589f37cea9111a51ca
samuel verstraete <samuel.verstraete@gmail.com>
2016-11-21 14:02:16 +0100

    getting rid of react-scripts

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
-import './App.css';
-import Gallery from './Gallery'
+import Gallery from './Gallery';
 
 class App extends Component {
   render() {
@@ -12,20 +11,20 @@ class App extends Component {
     //     .then(function (blob) {
     //          console.log(blob);
     //   });
+    // return (
+      //  <Gallery/>
+    // )
     return (
-       <Gallery/>
-    )
-    //return (
-    //   <div className="App">
-    //     <div className="App-header">
-    //       <img src={logo} className="App-logo" alt="logo" />
-    //       <h2>Welcome to React</h2>
-    //     </div>
-    //     <p className="App-intro">
-    //       To get started, edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //   </div>
-    // );
+       <div className="App">
+         <div className="App-header">
+           <img src={logo} className="App-logo" alt="logo" />
+           <h2>Welcome to React</h2>
+         </div>
+         <p className="App-intro">
+           To get started, edit <code>src/App.js</code> and save to reload.
+         </p>
+       </div>
+     );
   }
 }
 
